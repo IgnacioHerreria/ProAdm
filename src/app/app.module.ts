d71675f5db0f07c5eb4e7f187180ee6c9c99a63e
IgnacioHerreria <iherreria918@gmail..com>
2019-08-11 21:52:38 -0500

    routes pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
 
 //ROUTES
-import { APP_ROUTES } from './app.routes';
+import { APP_ROUTES } from "./app.routes";
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { GraphicsComponent } from './pages/graphics/graphics.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagenotfoundComponent } from './shared/pagenotfound/pagenotfound.component';
-import { PagesComponent } from './pages/pages.component';
-import { RegisterComponent } from './login/register.component';
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./login/register.component";
+import { PagesModule } from "./pages/pages.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    PagenotfoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    GraphicsComponent,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
-    RegisterComponent
+    RegisterComponent,
+
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    APP_ROUTES,
-  ],
+  imports: [BrowserModule, AppRoutingModule, APP_ROUTES, PagesModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
